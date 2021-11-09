b57a45a8589d38d1df6d10b0462d8df2056906ae
danikaharu <dani.kaharu17@gmail.com>
2021-11-09 16:20:31 +0800

    Add link login and profile

--- resources/views/layouts/user/partials/header.blade.php
+++ resources/views/layouts/user/partials/header.blade.php
@@ -10,9 +10,9 @@
         <nav id="navbar" class="navbar">
             <ul>
                 <li><a href="{{ route('home') }}" class="nav-link scrollto {{ request()->is('/') ? ' active' : '' }}">Beranda</a></li>
-                <li class="dropdown"><a href="#"><span>Profil</span> <i class="bi bi-chevron-down"></i></a>
+                <li class="dropdown"><a href="#" class="nav-link {{ request()->is('profil/*') ? ' active' : '' }}"><span>Profil</span> <i class="bi bi-chevron-down"></i></a>
                     <ul>
-                        <li><a href="#">Profil Instansi</a></li>
+                        <li><a href="{{ route('profil') }}">Profil Instansi</a></li>
                         <li><a href="#">Struktur Organisasi</a></li>
                         <li><a href="#">Tugas Pokok & Fungsi</a></li>
                         <li><a href="#">Unit Kerja & Jabatan</a></li>
@@ -20,14 +20,14 @@
                     </ul>
                 </li>
                 <li><a href="#">Regulasi</a></li>
-                <li class="dropdown"><a href="#" class="nav-link {{ request()->is('article') ? ' active' : '' }}"><span>Publikasi</span> <i class="bi bi-chevron-down"></i></a>
+                <li class="dropdown"><a href="#" class="nav-link {{ request()->is('berita') ? ' active' : '' }}"><span>Publikasi</span> <i class="bi bi-chevron-down"></i></a>
                     <ul>
-                        <li><a href="{{ route('article') }}">Berita</a></li>
+                        <li><a href="{{ route('berita') }}">Berita</a></li>
                         <li><a href="#">Galeri Kegiatan</a></li>
                     </ul>
                 </li>
                 <li><a href="#">Pengaduan</a></li>
-                <li><a class="getstarted" href="#">Login</a></li>
+                <li><a class="getstarted" href="{{ route('login') }}">Login</a></li>
             </ul>
             <i class="bi bi-list mobile-nav-toggle"></i>
         </nav><!-- .navbar -->
