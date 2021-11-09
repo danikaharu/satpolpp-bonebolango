6a5b032f01157392c7e9e57928b769ea53861657
danikaharu <dani.kaharu17@gmail.com>
2021-11-09 16:20:47 +0800

    Add Profile Page

---
+++ resources/views/layouts/user/profile.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.user.master')
+
+@section('title')Profil @endsection
+
+@section('content')
+<!-- ======= Breadcrumbs ======= -->
+<section class="breadcrumbs">
+    <div class="container">
+
+        <ol>
+            <li><a href="index.html">Beranda</a></li>
+            <li>Profil</li>
+        </ol>
+        <h2>Profil</h2>
+
+    </div>
+</section><!-- End Breadcrumbs -->
+
+<!-- ======= Blog Section ======= -->
+<section id="blog" class="blog">
+    <div class="container" data-aos="fade-up">
+
+        <h1>Ini adalah profil</h1>
+
+    </div>
+</section><!-- End Blog Section -->
+
+@endsection
