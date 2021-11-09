7b595bbc105778a3fccef0af4e8c784492279902
danikaharu <dani.kaharu17@gmail.com>
2021-11-09 15:57:55 +0800

    Change Hero Section

--- resources/views/layouts/user/dashboard.blade.php
+++ resources/views/layouts/user/dashboard.blade.php
@@ -8,8 +8,8 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-6 d-flex flex-column justify-content-center">
-                <h1 data-aos="fade-up">We offer modern solutions for growing your business</h1>
-                <h2 data-aos="fade-up" data-aos-delay="400">We are team of talented designers making websites with Bootstrap</h2>
+                <h1 data-aos="fade-up">Satpol PP Kabupaten Bone Bolango</h1>
+                <h2 data-aos="fade-up">Kami adalah garda terdepan penegakan peraturan daerah Bone Bolango</h2>
                 <div data-aos="fade-up" data-aos-delay="600">
                     <div class="text-center text-lg-start">
                         <a href="#about" class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">
@@ -20,7 +20,7 @@
                 </div>
             </div>
             <div class="col-lg-6 hero-img" data-aos="zoom-out" data-aos-delay="200">
-                <img src="{{ asset('assets_user/img/hero-img.png') }}" class="img-fluid" alt="">
+                <img src="{{ asset('assets_user/img/hero.png') }}" class="img-fluid" alt="">
             </div>
         </div>
     </div>
@@ -75,7 +75,6 @@
                 <div class="service-box blue">
                     <i class="ri-discuss-line icon"></i>
                     <h3>Bidang Penegakan Peraturan Perundang Undangan Daerah</h3>
-                    <a href="#" class="read-more"><span>Selengkapnya</span> <i class="bi bi-arrow-right"></i></a>
                 </div>
             </div>
 
@@ -83,7 +82,6 @@
                 <div class="service-box orange">
                     <i class="ri-discuss-line icon"></i>
                     <h3>Bidang Ketertiban Umum Dan Ketentraman Masyarakat</h3>
-                    <a href="#" class="read-more"><span>Selengkapnya</span> <i class="bi bi-arrow-right"></i></a>
                 </div>
             </div>
 
@@ -91,7 +89,6 @@
                 <div class="service-box green">
                     <i class="ri-discuss-line icon"></i>
                     <h3>Bidang Perlindungan Masyarakat</h3>
-                    <a href="#" class="read-more"><span>Selengkapnya</span> <i class="bi bi-arrow-right"></i></a>
                 </div>
             </div>
 
@@ -99,7 +96,6 @@
                 <div class="service-box red">
                     <i class="ri-discuss-line icon"></i>
                     <h3>Bidang Sumber Daya Aparatur</h3>
-                    <a href="#" class="read-more"><span>Selengkapnya</span> <i class="bi bi-arrow-right"></i></a>
                 </div>
             </div>
         </div>
