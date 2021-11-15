5e6192a3f0b171082162d9777c74505fb03ee288
danikaharu <dani.kaharu17@gmail.com>
2021-11-15 16:11:29 +0800

    Passing data to header

--- resources/views/layouts/user/partials/header.blade.php
+++ resources/views/layouts/user/partials/header.blade.php
@@ -12,11 +12,11 @@
                 <li><a href="{{ route('home') }}" class="nav-link scrollto {{ request()->is('/') ? ' active' : '' }}">Beranda</a></li>
                 <li class="dropdown"><a href="#" class="nav-link {{ request()->is('profil/*') ? ' active' : '' }}"><span>Profil</span> <i class="bi bi-chevron-down"></i></a>
                     <ul>
-                        <li><a href="{{ route('profil') }}">Profil Instansi</a></li>
-                        <li><a href="#">Struktur Organisasi</a></li>
-                        <li><a href="#">Tugas Pokok & Fungsi</a></li>
-                        <li><a href="#">Unit Kerja & Jabatan</a></li>
-                        <li><a href="#">Visi & Misi</a></li>
+                        @foreach ($profile as $data)
+                        <li>
+                            <a href="{{ route('profil', $data->slug) }}">{{ $data->title }}</a>
+                        </li>
+                        @endforeach
                     </ul>
                 </li>
                 <li><a href="{{ route('regulasi') }}" class="nav-link {{ request()->is('regulasi') ? ' active' : '' }}">Regulasi</a></li>
