7727351e60f8957f7b6f109544ee29ff445fb338
danikaharu <dani.kaharu17@gmail.com>
2021-11-15 10:09:51 +0800

    Fix News View

--- resources/views/layouts/user/news.blade.php
+++ resources/views/layouts/user/news.blade.php
@@ -17,50 +17,44 @@
 </section><!-- End Breadcrumbs -->
 
 <!-- ======= Blog Section ======= -->
-<section class="blog">
+<section id="blog" class="blog">
     <div class="container" data-aos="fade-up">
-        <header class="section-header">
-            <h2>Satpol PP Kabupaten Bone Bolango</h2>
-            <p>Berita</p>
-        </header>
 
         <div class="row">
 
             <div class="col-lg-8 entries">
-
+                @foreach ($news as $data)
                 <article class="entry">
-                    @foreach ($news as $data )
+
                     <div class="entry-img">
-                        <img src="{{ Storage::url('news/'.$data->image) }}" alt="" class="img-fluid">
+                        <img src="{{ asset('assets_user/img/blog/blog-1.jpg') }}" alt="" class="img-fluid">
                     </div>
 
                     <h2 class="entry-title">
-                        <a href="#">{!! Str::words($data->title,5) !!}</a>
+                        <a href="#">{{ $data->title }}</a>
                     </h2>
 
                     <div class="entry-meta">
                         <ul>
                             <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a href="#">Admin</a></li>
-                            <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a href="#"><time datetime="2020-01-01">{{ $data->created_at->format('d M Y') }}</time></a></li>
+                            <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a href="#"><time datetime="{{ $data->created_at->format('d M Y H:i') }}">{{ $data->created_at->format('d M Y H:i') }}</time></a></li>
                         </ul>
                     </div>
 
                     <div class="entry-content">
-                        {!! Str::limit($data->body, 300) !!}
+                        <p>
+                            {{ Illuminate\Support\Str::words(strip_tags($data->body) , 50) }}
+                        </p>
                         <div class="read-more">
                             <a href="#">Selengkapnya</a>
                         </div>
                     </div>
-                    @endforeach
-                </article><!-- End blog entry -->
 
-                {{-- @if ($news->isEmpty())
-                <h2>Maaf, data belum ada</h2>
-                @else
-               
-                @endif --}}
+                </article><!-- End blog entry -->
+                @endforeach
 
                 {{ $news->links('vendor.pagination.custom') }}
+
             </div><!-- End blog entries list -->
 
             <div class="col-lg-4">
@@ -77,35 +71,13 @@
 
                     <h3 class="sidebar-title">Berita Terkini</h3>
                     <div class="sidebar-item recent-posts">
+                        @foreach ($news as $data)
                         <div class="post-item clearfix">
                             <img src="{{ asset('assets_user/img/blog/blog-recent-1.jpg') }}" alt="">
-                            <h4><a href="blog-single.html">Nihil blanditiis at in nihil autem</a></h4>
-                            <time datetime="2020-01-01">Jan 1, 2020</time>
+                            <h4><a href="#">{{ $data->title }}</a></h4>
+                            <time datetime="{{ $data->created_at->format('d M Y H:i') }}">{{ $data->created_at->format('d M Y H:i') }}</time>
                         </div>
-
-                        <div class="post-item clearfix">
-                            <img src="{{ asset('assets_user/img/blog/blog-recent-2.jpg') }}" alt="">
-                            <h4><a href="blog-single.html">Quidem autem et impedit</a></h4>
-                            <time datetime="2020-01-01">Jan 1, 2020</time>
-                        </div>
-
-                        <div class="post-item clearfix">
-                            <img src="{{ asset('assets_user/img/blog/blog-recent-3.jpg') }}" alt="">
-                            <h4><a href="blog-single.html">Id quia et et ut maxime similique occaecati ut</a></h4>
-                            <time datetime="2020-01-01">Jan 1, 2020</time>
-                        </div>
-
-                        <div class="post-item clearfix">
-                            <img src="{{ asset('assets_user/img/blog/blog-recent-4.jpg') }}" alt="">
-                            <h4><a href="blog-single.html">Laborum corporis quo dara net para</a></h4>
-                            <time datetime="2020-01-01">Jan 1, 2020</time>
-                        </div>
-
-                        <div class="post-item clearfix">
-                            <img src="{{ asset('assets_user/img/blog/blog-recent-5.jpg') }}" alt="">
-                            <h4><a href="blog-single.html">Et dolores corrupti quae illo quod dolor</a></h4>
-                            <time datetime="2020-01-01">Jan 1, 2020</time>
-                        </div>
+                        @endforeach
 
                     </div><!-- End sidebar recent posts-->
 
