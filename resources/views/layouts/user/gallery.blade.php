44ad29b2c4cbfae4b42ff6d1684a450763a41abd
danikaharu <dani.kaharu17@gmail.com>
2021-11-15 10:09:38 +0800

    Create View Gallery User

--- resources/views/layouts/user/gallery.blade.php
+++ resources/views/layouts/user/gallery.blade.php
@@ -28,94 +28,21 @@
 
         <div class="row gy-4 portfolio-container" data-aos="fade-up" data-aos-delay="200">
 
+            @foreach ($gallery as $data)
             <div class="col-lg-4 col-md-6 portfolio-item ">
                 <div class="portfolio-wrap">
                     <img src="{{ asset('assets_user/img/portfolio/portfolio-1.jpg') }}" class="img-fluid" alt="">
                     <div class="portfolio-info">
-                        <h4>App 1</h4>
-                        <p>App</p>
+                        <h4>{{ $data->title }}</h4>
                         <div class="portfolio-links">
-                            <a href="{{ asset('assets_user/img/portfolio/portfolio-1.jpg') }}" data-gallery="portfolioGallery" class="portfokio-lightbox" title="App 1"><i class="bi bi-plus"></i></a>
+                            <a href="{{ asset('assets_user/img/portfolio/portfolio-1.jpg') }}" data-gallery="portfolioGallery" class="portfokio-lightbox" title="{{ $data->title }}"><i class="bi bi-plus"></i></a>
                         </div>
                     </div>
                 </div>
             </div>
+            @endforeach
 
-            <div class="col-lg-4 col-md-6 portfolio-item ">
-                <div class="portfolio-wrap">
-                    <img src="{{ asset('assets_user/img/portfolio/portfolio-1.jpg') }}" class="img-fluid" alt="">
-                    <div class="portfolio-info">
-                        <h4>App 1</h4>
-                        <p>App</p>
-                        <div class="portfolio-links">
-                            <a href="{{ asset('assets_user/img/portfolio/portfolio-1.jpg') }}" data-gallery="portfolioGallery" class="portfokio-lightbox" title="App 1"><i class="bi bi-plus"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 portfolio-item ">
-                <div class="portfolio-wrap">
-                    <img src="{{ asset('assets_user/img/portfolio/portfolio-1.jpg') }}" class="img-fluid" alt="">
-                    <div class="portfolio-info">
-                        <h4>App 1</h4>
-                        <p>App</p>
-                        <div class="portfolio-links">
-                            <a href="{{ asset('assets_user/img/portfolio/portfolio-1.jpg') }}" data-gallery="portfolioGallery" class="portfokio-lightbox" title="App 1"><i class="bi bi-plus"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 portfolio-item ">
-                <div class="portfolio-wrap">
-                    <img src="{{ asset('assets_user/img/portfolio/portfolio-1.jpg') }}" class="img-fluid" alt="">
-                    <div class="portfolio-info">
-                        <h4>App 1</h4>
-                        <p>App</p>
-                        <div class="portfolio-links">
-                            <a href="{{ asset('assets_user/img/portfolio/portfolio-1.jpg') }}" data-gallery="portfolioGallery" class="portfokio-lightbox" title="App 1"><i class="bi bi-plus"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 portfolio-item ">
-                <div class="portfolio-wrap">
-                    <img src="{{ asset('assets_user/img/portfolio/portfolio-1.jpg') }}" class="img-fluid" alt="">
-                    <div class="portfolio-info">
-                        <h4>App 1</h4>
-                        <p>App</p>
-                        <div class="portfolio-links">
-                            <a href="{{ asset('assets_user/img/portfolio/portfolio-1.jpg') }}" data-gallery="portfolioGallery" class="portfokio-lightbox" title="App 1"><i class="bi bi-plus"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 portfolio-item ">
-                <div class="portfolio-wrap">
-                    <img src="{{ asset('assets_user/img/portfolio/portfolio-1.jpg') }}" class="img-fluid" alt="">
-                    <div class="portfolio-info">
-                        <h4>App 1</h4>
-                        <p>App</p>
-                        <div class="portfolio-links">
-                            <a href="{{ asset('assets_user/img/portfolio/portfolio-1.jpg') }}" data-gallery="portfolioGallery" class="portfokio-lightbox" title="App 1"><i class="bi bi-plus"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="blog">
-            <div class="blog-pagination">
-                <ul class="justify-content-center">
-                    <li><a href="#">1</a></li>
-                    <li class="active"><a href="#">2</a></li>
-                    <li><a href="#">3</a></li>
-                </ul>
-            </div>
-        </div>
+            {{ $gallery->links('vendor.pagination.custom') }}
 
 
 </section><!-- End Portfolio Section -->
