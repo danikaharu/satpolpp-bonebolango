f37a4736e9ec4e897cd1fe49b203c09c90858720
danikaharu <dani.kaharu17@gmail.com>
2021-11-15 22:45:20 +0800

    create function show News

--- app/Http/Controllers/User/NewsController.php
+++ app/Http/Controllers/User/NewsController.php
@@ -10,7 +10,15 @@ class NewsController extends Controller
 {
     public function index()
     {
-        $news = News::latest()->paginate(4);
-        return view('layouts.user.news', compact('news'));
+        $news1 = News::latest()->paginate(4);
+        $news2 = News::latest()->limit(4)->get();
+        return view('layouts.user.news', compact('news1', 'news2'));
+    }
+
+    public function show($slug)
+    {
+        $news = News::where('slug', $slug)->first();
+        $recentNews = News::latest()->limit(4)->get();
+        return view('layouts.user.newsDetail', compact('news', 'recentNews'));
     }
 }
