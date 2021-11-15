a44431c119ea0e6ce437cbf009657e2fb463bf89
danikaharu <dani.kaharu17@gmail.com>
2021-11-15 13:39:56 +0800

    Display Data to sidebar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        \View::composer('layouts.admin.partials.sidebar', function ($view) {
+            $profile = DB::table('profiles')->select('title', 'slug')->get();
+            $view->with(['data' => $profile]);
+        });
     }
 }
