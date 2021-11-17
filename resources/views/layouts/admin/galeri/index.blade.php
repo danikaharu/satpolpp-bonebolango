40203c4448a30c32be114f99d52f9bc47d63a8f1
danikaharu <dani.kaharu17@gmail.com>
2021-11-17 12:53:16 +0800

    Fix View Gallery

--- resources/views/layouts/admin/galeri/index.blade.php
+++ resources/views/layouts/admin/galeri/index.blade.php
@@ -3,48 +3,55 @@
 @section('title', 'Galeri Kegiatan | Admin Satpol PP Bone Bolango')
 
 @section('content')
+<div class="page-heading">
+    <h3>Galeri Kegiatan</h3>
+</div>
 
-<ol class="breadcrumb mb-4">
-    <h4>
-        <li class="breadcrumb-item active fon">Galeri Kegiatan</li>
-    </h4>
-</ol>
+<div class="page-content">
+    <section class="section">
+        <div class="card">
+            <div class="card-header">
+                <a href="{{ route('galery.create') }}" class="btn btn-primary">Tambah Galeri</a>
+            </div>
+            <div class="card-content">
+                <div class="table-responsive">
+                    <table id="pengaduanTable" class="table table-hover table-responsive">
+                        <thead>
+                            <tr class="text-center">
+                                <th>No</th>
+                                <th>Judul Galeri</th>
+                                <th>Isi Galeri</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($galeries as $item)
 
-<a href="{{ route('galery.create') }}" class="btn btn-primary mb-4">Tambah Galeri</a>
-<table id="pengaduanTable" class="table">
-    <thead>
-        <tr class="text-center">
-            <th>No</th>
-            <th>Gambar</th>
-            <th>Judul Galeri</th>
-            <th>Isi Galeri</th>
-            <th>Aksi</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($galeries as $v => $item)
+                            <tr class="text-center">
+                                <td>{{$loop->iteration}}</td>
+                                <td>{{$item->title}}</td>
+                                <td>{!! Str::limit($item->body,20) !!}</td>
+                                <td>
 
-        <tr class="text-center">
-            <td>{{$v += 1}}</td>
-            <td>
-                <img src="{{ Storage::url('galery/'.$item->image) }}" class="rounded" style="height: 150px">
-            </td>
-            <td>{{$item->title}}</td>
-            <td>{!! Str::limit($item->body,20) !!}</td>
-            <td>
+                                    <form onsubmit="return confirm('Apakah Anda Yakin ?');" action="{{ route('galery.destroy', $item->id) }}" method="POST">
+                                        <a href="{{ Storage::url('galery/'.$item->image) }}" target="_blank" class="btn btn-sm btn-success my-2">Gambar</a>
+                                        <a href="{{ route('galery.edit', $item->slug) }}" class="btn btn-sm btn-primary my-2">Edit</a>
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-sm btn-danger my-2">Hapus</button>
+                                    </form>
 
-                <form onsubmit="return confirm('Apakah Anda Yakin ?');" action="{{ route('galery.destroy', $item->id) }}" method="POST">
-                    <a href="{{ route('galery.edit', $item->slug) }}" class="btn btn-sm btn-primary">EDIT</a>
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="btn btn-sm btn-danger">HAPUS</button>
-                </form>
+                                </td>
+                            </tr>
+                            @endforeach ($galeries as $galeri)
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
 
-            </td>
-        </tr>
-        @endforeach ($galeries as $galeri)
-    </tbody>
-</table>
 
 
 @endsection
