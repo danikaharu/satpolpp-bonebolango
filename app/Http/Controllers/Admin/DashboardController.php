97e9bf747f08a65b1c937f46199e5d00f8840529
danikaharu <dani.kaharu17@gmail.com>
2021-11-18 11:37:14 +0800

    Create Dashboard Admin

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Admin\Complaint;
+use App\Models\Admin\Galery;
+use App\Models\Admin\News;
+use App\Models\Admin\Regulation;
 
 class DashboardController extends Controller
 {
@@ -18,6 +22,12 @@ class DashboardController extends Controller
 
     public function index()
     {
-        return view('layouts.admin.dashboard');
+        $totalComplaint = Complaint::count();
+        $totalRegulation = Regulation::count();
+        $totalGallery = Galery::count();
+        $totalNews = News::count();
+        $totalComplaintInProcess = Complaint::where('status', '2')->count();
+        $totalComplaintCompleted = Complaint::where('status', '3')->count();
+        return view('layouts.admin.dashboard', compact('totalComplaint', 'totalRegulation', 'totalGallery', 'totalNews',  'totalComplaintInProcess', 'totalComplaintCompleted'));
     }
 }
