35a8d6014f8dd5a86040c5f7f0da90ebbb4c15c3
danikaharu <dani.kaharu17@gmail.com>
2021-11-18 16:58:46 +0800

    Relation Table Response

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Admin\Response;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -42,4 +43,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function response()
+    {
+        $this->hasOne(Response::class);
+    }
 }
