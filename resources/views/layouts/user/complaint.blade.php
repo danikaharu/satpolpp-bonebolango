6eca5acdc23bb8f3f2894406d04a30805e9674eb
danikaharu <dani.kaharu17@gmail.com>
2021-11-18 16:59:20 +0800

    Add view Complaint User

--- resources/views/layouts/user/complaint.blade.php
+++ resources/views/layouts/user/complaint.blade.php
@@ -26,46 +26,98 @@
             <p>Pengaduan</p>
         </header>
 
+        <section class="contact">
+            <div class="col-lg-12">
+                <form action="{{ route('complaint.store') }}" method="POST" enctype="multipart/form-data" style="background: #fafbff;
+                                                                                                                 padding: 30px;
+                                                                                                                 height: 100%;">
+                    @csrf
+                    <div class="row gy-4">
+
+                        <div class="col-md-12">
+                            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" value="{{ old('name') }}" placeholder="Nama">
+                            @error('name')
+                            <div class="alert alert-danger mt-2">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                        </div>
+                        <div class="col-md-12 ">
+                            <input type="email" class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}" name="email" placeholder="Email">
+                            @error('email')
+                            <div class="alert alert-danger mt-2">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                        </div>
+                        <div class="col-md-12">
+                            <input type="text" class="form-control @error('title') is-invalid @enderror" value="{{ old('title') }}" name="title" placeholder="Judul Aduan">
+                            @error('title')
+                            <div class="alert alert-danger mt-2">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                        </div>
+
+                        <div class="col-md-12">
+                            <textarea class="form-control @error('description') is-invalid @enderror" value="{{ old('description') }}" name="description" rows="6" placeholder="Tuliskan Aduan Anda"></textarea>
+                            @error('description')
+                            <div class="alert alert-danger mt-2">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                        </div>
+                        <div class="col-md-12 text-center">
+                            <button type="submit" style="background: #4154f1;
+                                                         border: 0;
+                                                         padding: 10px 30px;
+                                                         color: #fff;
+                                                         transition: 0.4s;
+                                                         border-radius: 4px;">
+                                Simpan
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </section>
+
         <div class="row gy-4" data-aos="fade-left">
-
-            <div class="col-lg-4 col-md-6" data-aos="zoom-in" data-aos-delay="100">
-
+            <h3 style="color: #012970;">Daftar Aduan</h3>
+            @if($complaint->isEmpty())
+            <h2>Maaf, aduan belum ada</h2>
+            @else
+            @foreach ($complaint as $data)
+            <div class="col-lg-6 col-md-6" data-aos="zoom-in" data-aos-delay="100">
                 <article class="entry">
 
                     <h2 class="entry-title">
-                        <a href="#">Judul Laporan</a>
+                        <a href="#">{{ $data->title }}</a>
                     </h2>
 
                     <div class="entry-meta">
                         <ul>
-                            <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a href="#">Pelapor</a></li>
-                            <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a href="#"><time>Jan 1, 2020</time></a></li>
+                            <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a href="#">{{ $data->name }}</a></li>
+                            <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a href="#"><time>{{ $data->created_at->format('d M Y H:i') }}</time></a></li>
                         </ul>
                     </div>
 
                     <div class="entry-content">
                         <p>
-                            Deskripsi Laporan
+                            {{ Illuminate\Support\Str::words(strip_tags($data->description) , 5) }}
                         </p>
                         <div class="read-more">
-                            <a href="#">Selengkapnya</a>
+                            <a href="{{ route('pengaduan.detail', $data->slug) }}">Selengkapnya</a>
                         </div>
                     </div>
 
                 </article><!-- End blog entry -->
-
             </div>
-
-
+            @endforeach
+            @endif
         </div>
 
-        <div class="blog-pagination">
-            <ul class="justify-content-center">
-                <li><a href="#">1</a></li>
-                <li class="active"><a href="#">2</a></li>
-                <li><a href="#">3</a></li>
-            </ul>
-        </div>
+        {{ $complaint->links('vendor.pagination.custom') }}
 
     </div>
 
