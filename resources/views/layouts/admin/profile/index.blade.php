ae825fb946adee57c8321cd5599315ffaeec30c2
danikaharu <dani.kaharu17@gmail.com>
2023-08-08 14:51:10 +0800

    remove toolbar option

--- resources/views/layouts/admin/profile/index.blade.php
+++ resources/views/layouts/admin/profile/index.blade.php
@@ -102,16 +102,7 @@
         $(document).ready(function() {
             $('#summernote').summernote({
                 height: 200,
-                placeholder: 'Harap Masukan Isi Galeri Kegiatan',
-                toolbar: [
-                    // [groupName, [list of button]]
-                    ['style', ['bold', 'italic', 'underline', 'clear']],
-                    ['font', ['strikethrough', 'superscript', 'subscript']],
-                    ['fontsize', ['fontsize']],
-                    ['color', ['color']],
-                    ['para', ['ul', 'ol', 'paragraph']],
-                    ['height', ['height']]
-                ],
+                placeholder: 'Harap Masukan Isi Profil',
             });
         });
     </script>
